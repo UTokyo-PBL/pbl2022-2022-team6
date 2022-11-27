1d4ef0619da318f2ab97679df40235210f64d0e3
ms3744 <ms22sharma@gmail.com>
2022-11-27 11:00:07 +0900

    adding service registeration (PWA)

--- translango/src/index.tsx
+++ translango/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppCtx, { AppContextInterface } from './store/app-state-context';
 import { CssBaseline } from '@mui/material';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -27,3 +28,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
