d78e1576080694faa25ba82d7517e1b026a8cdeb
gaurishg <gaurishgangwar@gmail.com>
2022-12-08 07:41:24 +0000

    Used Generalized responsive component

--- translango/src/pages/sign-up/create-profile/CreateProfile.tsx
+++ translango/src/pages/sign-up/create-profile/CreateProfile.tsx
@@ -1,39 +1,40 @@
 import {
-    AccountCircle,
-    Add,
-    Google,
-    Language,
-    Person,
-  } from "@mui/icons-material";
-  import {
-    Avatar,
-    Button,
-    Box,
-    Checkbox,
-    Divider,
-    Fab,
-    FormControlLabel,
-    InputAdornment,
-    Stack,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  import SignUpAndLoginTop from "../SignUpAndLoginTop";
-  
-  const CreateProfile: React.FC = () => {
-    return (
+  AccountCircle,
+  Add,
+  Google,
+  Language,
+  Person,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Button,
+  Box,
+  Checkbox,
+  Divider,
+  Fab,
+  FormControlLabel,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import FillPageWithSidePic from "../../../components/FillPageWithSidePic";
+import SignUpAndLoginTop from "../SignUpAndLoginTop";
+
+const CreateProfile: React.FC = () => {
+  return (
+    <FillPageWithSidePic>
       <Stack>
         <SignUpAndLoginTop
           backTo="/sign-up/confirm-email"
           title="Let's Set Up"
           subtitle="Create your profile here"
         />
-  
+
         <Stack component="form" p={4} spacing={2}>
-          <Box marginX={'auto'}>
+          <Box marginX={"auto"}>
             <Avatar
               sx={{
-                  
                 width: { xs: "150px", sm: "300px" },
                 height: { xs: "150px", sm: "300px" },
               }}
@@ -59,7 +60,7 @@ import {
               {/* <Chip label="+" color="secondary" onClick={() => {}} /> */}
             </Box>
           </Box>
-  
+
           <TextField
             fullWidth
             variant="standard"
@@ -72,7 +73,7 @@ import {
               ),
             }}
           />
-  
+
           <TextField
             fullWidth
             variant="standard"
@@ -87,7 +88,7 @@ import {
               ),
             }}
           />
-  
+
           <TextField
             fullWidth
             variant="standard"
@@ -100,35 +101,35 @@ import {
               ),
             }}
           />
-  
+
           <FormControlLabel
             sx={{ width: "100%" }}
             control={<Checkbox defaultChecked />}
             label="Keep me signed in"
           />
-  
+
           <FormControlLabel
             sx={{ width: "100%" }}
             control={<Checkbox defaultChecked />}
             label="Email me about special pricing and more"
           />
-  
+
           <Button variant="contained" fullWidth>
             Create Account
           </Button>
-  
+
           <Divider>Or sign in with</Divider>
-  
+
           <Button startIcon={<Google />}>Google</Button>
-  
+
           <Button>
             Already have an account?{" "}
             <Typography variant="subtitle2">Log in</Typography>
           </Button>
         </Stack>
       </Stack>
-    );
-  };
-  
-  export default CreateProfile;
-  
+    </FillPageWithSidePic>
+  );
+};
+
+export default CreateProfile;
--- translango/src/pages/sign-up/get-sign-up-details/GetSignUpDetails.tsx
+++ translango/src/pages/sign-up/get-sign-up-details/GetSignUpDetails.tsx
@@ -4,114 +4,96 @@ import {
   Button,
   Chip,
   Divider,
-  Grid,
   InputAdornment,
   Stack,
   TextField,
 } from "@mui/material";
+import FillPageWithSidePic from "../../../components/FillPageWithSidePic";
 import SignUpAndLoginTop from "../SignUpAndLoginTop";
 
 const GetSignUpDetails: React.FC = () => {
   return (
-    <Grid container sx={{ height: '100vh' }}>
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={7}
-        sx={{
-          backgroundImage: "url(https://source.unsplash.com/random)",
-          backgroundRepeat: "no-repeat",
-          backgroundColor: (t) =>
-            t.palette.mode === "light"
-              ? t.palette.grey[50]
-              : t.palette.grey[900],
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      />
-      <Grid item xs={12} sm={8} md={5} >
-        <Box>
-          <SignUpAndLoginTop
-            backTo="/"
-            title="Let's register"
-            subtitle="Discovery awaits!"
+    <FillPageWithSidePic>
+      <Box>
+        <SignUpAndLoginTop
+          backTo="/"
+          title="Let's register"
+          subtitle="Discovery awaits!"
+        />
+        <Stack component="form" p={2} spacing={4}>
+          <TextField
+            fullWidth
+            variant="standard"
+            placeholder="example@translango.com"
+            type="email"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Email />
+                </InputAdornment>
+              ),
+            }}
           />
-          <Stack component="form" p={2} spacing={4}>
-            <TextField
-              fullWidth
-              variant="standard"
-              placeholder="example@translango.com"
-              type="email"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Email />
-                  </InputAdornment>
-                ),
-              }}
-            />
 
-            <TextField
-              fullWidth
-              variant="standard"
-              type="password"
-              placeholder="password"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Lock />
-                  </InputAdornment>
-                ),
-              }}
-            />
+          <TextField
+            fullWidth
+            variant="standard"
+            type="password"
+            placeholder="password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Lock />
+                </InputAdornment>
+              ),
+            }}
+          />
 
-            <TextField
-              fullWidth
-              variant="standard"
-              type="password"
-              placeholder="confirm password"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Lock />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <Chip
-              label={
-                <p>
-                  We keep you safe! Our password is encrypted and inaccessible
-                  to anyone excpept you!
-                </p>
-              }
-              color="success"
-              // sx={{
-              //   bgcolor: "success.light",
-              //   width: "100%",
-              //   overflowWrap: 'break-word'
-              //   // height: 'auto'
-              // }}
-            />
+          <TextField
+            fullWidth
+            variant="standard"
+            type="password"
+            placeholder="confirm password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Lock />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Chip
+            label={
+              <p>
+                We keep you safe! Our password is encrypted and inaccessible to
+                anyone excpept you!
+              </p>
+            }
+            color="success"
+            // sx={{
+            //   bgcolor: "success.light",
+            //   width: "100%",
+            //   overflowWrap: 'break-word'
+            //   // height: 'auto'
+            // }}
+          />
 
-            <Button
-              // sx={{p: 1.5}}
-              variant="contained"
-            >
-              Sign Up
-            </Button>
-            <Divider>OR</Divider>
-            <Button
-              variant="outlined"
-              // sx={{p: 1}}
-            >
-              Login
-            </Button>
-          </Stack>
-        </Box>
-      </Grid>
-    </Grid>
+          <Button
+            // sx={{p: 1.5}}
+            variant="contained"
+          >
+            Sign Up
+          </Button>
+          <Divider>OR</Divider>
+          <Button
+            variant="outlined"
+            // sx={{p: 1}}
+          >
+            Login
+          </Button>
+        </Stack>
+      </Box>
+    </FillPageWithSidePic>
   );
 };
 
