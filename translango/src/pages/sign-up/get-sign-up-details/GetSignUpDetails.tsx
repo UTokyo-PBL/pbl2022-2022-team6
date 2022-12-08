87e6409789cf636d7e1e6da4568581c5e4b8cac3
gaurishg <gaurishgangwar@gmail.com>
2022-12-08 07:22:22 +0000

    Made GetSignUpDetails responsive

--- translango/src/pages/sign-up/get-sign-up-details/GetSignUpDetails.tsx
+++ translango/src/pages/sign-up/get-sign-up-details/GetSignUpDetails.tsx
@@ -4,101 +4,114 @@ import {
   Button,
   Chip,
   Divider,
+  Grid,
   InputAdornment,
   Stack,
   TextField,
-  Typography,
 } from "@mui/material";
 import SignUpAndLoginTop from "../SignUpAndLoginTop";
 
 const GetSignUpDetails: React.FC = () => {
   return (
-    <Box>
-      <SignUpAndLoginTop
-        backTo="/"
-        title="Let's register"
-        subtitle="Discovery awaits!"
+    <Grid container sx={{ height: '100vh' }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       />
-      <Stack component="form" p={2} spacing={4}>
-        <TextField
-          fullWidth
-          variant="standard"
-          placeholder="example@translango.com"
-          type="email"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Email />
-              </InputAdornment>
-            ),
-          }}
-        />
+      <Grid item xs={12} sm={8} md={5} >
+        <Box>
+          <SignUpAndLoginTop
+            backTo="/"
+            title="Let's register"
+            subtitle="Discovery awaits!"
+          />
+          <Stack component="form" p={2} spacing={4}>
+            <TextField
+              fullWidth
+              variant="standard"
+              placeholder="example@translango.com"
+              type="email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-        <TextField
-          fullWidth
-          variant="standard"
-          type="password"
-          placeholder="password"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Lock />
-              </InputAdornment>
-            ),
-          }}
-        />
+            <TextField
+              fullWidth
+              variant="standard"
+              type="password"
+              placeholder="password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Lock />
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-        <TextField
-          fullWidth
-          variant="standard"
-          type="password"
-          placeholder="confirm password"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Lock />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Chip
-          label={
-            <p>
-              We keep you safe! Our password is encrypted and inaccessible to
-              anyone excpept you!
-            </p>
-          }
-          color="success"
-          // sx={{
-          //   bgcolor: "success.light",
-          //   width: "100%",
-          //   overflowWrap: 'break-word'
-          //   // height: 'auto'
-          // }}
-        />
+            <TextField
+              fullWidth
+              variant="standard"
+              type="password"
+              placeholder="confirm password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Lock />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Chip
+              label={
+                <p>
+                  We keep you safe! Our password is encrypted and inaccessible
+                  to anyone excpept you!
+                </p>
+              }
+              color="success"
+              // sx={{
+              //   bgcolor: "success.light",
+              //   width: "100%",
+              //   overflowWrap: 'break-word'
+              //   // height: 'auto'
+              // }}
+            />
 
-        <Box bgcolor="success.light" borderRadius="20px">
-          <Typography p={1} color="white">
-            We keep you safe! Our password is encrypted and inaccessible to
-            anyone excpept you!
-          </Typography>
+            <Button
+              // sx={{p: 1.5}}
+              variant="contained"
+            >
+              Sign Up
+            </Button>
+            <Divider>OR</Divider>
+            <Button
+              variant="outlined"
+              // sx={{p: 1}}
+            >
+              Login
+            </Button>
+          </Stack>
         </Box>
-
-        <Button
-          // sx={{p: 1.5}}
-          variant="contained"
-        >
-          Sign Up
-        </Button>
-        <Divider>OR</Divider>
-        <Button
-          variant="outlined"
-          // sx={{p: 1}}
-        >
-          Login
-        </Button>
-      </Stack>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 
