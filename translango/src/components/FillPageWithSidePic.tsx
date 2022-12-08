dc886f1dcc59eaddc1842d9040414eb6ab25bea2
gaurishg <gaurishgangwar@gmail.com>
2022-12-08 07:40:10 +0000

    Added FillPageWithSidePic to make pages responsive

---
+++ translango/src/components/FillPageWithSidePic.tsx
@@ -0,0 +1,30 @@
+import { Grid } from "@mui/material";
+import { PropsWithChildren } from "react";
+
+const FillPageWithSidePic: React.FC<PropsWithChildren> = (props) => {
+  return (
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5}>
+        {props.children}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default FillPageWithSidePic;
