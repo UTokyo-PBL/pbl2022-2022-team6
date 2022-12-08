3a2958d6ad14b433ec194a6e7a549748442fd238
gaurishg <gaurishgangwar@gmail.com>
2022-12-08 09:11:24 +0000

    used OTPInput Element

--- translango/src/pages/sign-up/confirm-email/ConfirmEmail.tsx
+++ translango/src/pages/sign-up/confirm-email/ConfirmEmail.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Divider, OutlinedInput, Stack, Typography } from "@mui/material";
 import { useContext } from "react";
 import FillPageWithSidePic from "../../../components/FillPageWithSidePic";
+import OTPInput from "../../../components/OTPInput";
 import AppCtx from "../../../store/app-state-context";
 import SignUpAndLoginTop from "../SignUpAndLoginTop";
 import ConfirmEmailPic from "./confirm-email-logo.svg";
@@ -22,12 +23,7 @@ const ConfirmEmail: React.FC = () => {
         </Typography>
         <Typography variant='h6' component='p' gutterBottom>({ctx.email || "example@translango.com"})</Typography>
 
-        <Stack px={8} my={2} divider={<Divider orientation="vertical" flexItem />} direction='row' spacing={2} justifyContent='center'>
-            <OutlinedInput sx={{maxWidth: '60px'}} type="number"/>
-            <OutlinedInput sx={{maxWidth: '60px'}} type="number"/>
-            <OutlinedInput sx={{maxWidth: '60px'}} type="number"/>
-            <OutlinedInput sx={{maxWidth: '60px'}} type="number"/>
-        </Stack>
+        <OTPInput inputLength={4}></OTPInput>
 
         <Button fullWidth variant='contained'>Submit</Button>
       </Stack>
