a3547db44af9bc7f8be61ca201a1040827e56459
gaurishg <gaurishgangwar@gmail.com>
2023-01-12 10:16:12 +0000

    Added arabic and french

--- translango/src/components/selectLanguage.tsx
+++ translango/src/components/selectLanguage.tsx
@@ -1,49 +1,51 @@
 import { FormControl, MenuItem } from "@mui/material";
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useContext } from "react";
 import AppCtx, { AppCtxUpdater } from "../store/app-state-context";
 
-
 export const uiLanguages = [
-    { value: 'en', text: "English" },
-    { value: 'zh', text: "中文" },
-    { value: 'hi', text: "हिन्दी" },
-    { value: 'es', text: "español" },
-    { value: 'ja', text: "日本語" },
+  { value: "ar", text: "عربى" },
+  { value: "en", text: "English" },
+  { value: "es", text: "español" },
+  { value: "fr", text: "française" },
+  { value: "hi", text: "हिन्दी" },
+  { value: "ja", text: "日本語" },
+  { value: "zh", text: "中文" },
 ];
 
-
 export default function SelectLanguage() {
-    // It is a hook imported from 'react-i18next'
-    // const { t } = useTranslation();
-    const ctx = useContext(AppCtx);
-    const ctxUpdater = useContext(AppCtxUpdater);
+  // It is a hook imported from 'react-i18next'
+  // const { t } = useTranslation();
+  const ctx = useContext(AppCtx);
+  const ctxUpdater = useContext(AppCtxUpdater);
 
-    const handleChange = (event: SelectChangeEvent) => {
-        ctx.nativeLanguage = event.target.value;
-        ctxUpdater({...ctx});
-    };
+  const handleChange = (event: SelectChangeEvent) => {
+    ctx.nativeLanguage = event.target.value;
+    ctxUpdater({ ...ctx });
+  };
 
-    return (
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <Select
-                    value={ctx.nativeLanguage}
-                    onChange={handleChange}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                    labelId="demo-simple-select-autowidth-label"
-                    id="demo-simple-select-autowidth"
-                    sx={{
-                        bgcolor: 'white',
-                        color: 'primary'
-                    }}
-                >
-                    {uiLanguages.map(item => {
-                        return (<MenuItem key={item.value}
-                            value={item.value}>{item.text}</MenuItem>);
-                    })}
-                </Select>
-            </FormControl>
-    );
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <Select
+        value={ctx.nativeLanguage}
+        onChange={handleChange}
+        displayEmpty
+        inputProps={{ "aria-label": "Without label" }}
+        labelId="demo-simple-select-autowidth-label"
+        id="demo-simple-select-autowidth"
+        sx={{
+          bgcolor: "white",
+          color: "primary",
+        }}
+      >
+        {uiLanguages.map((item) => {
+          return (
+            <MenuItem key={item.value} value={item.value}>
+              {item.text}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </FormControl>
+  );
 }
-
